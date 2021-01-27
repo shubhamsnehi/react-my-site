74cb89afd6afa5885f79bf37afe91a64884191e3
Shubham Snehi <ssnehi144@gmail.com>
2021-01-27 11:17:53 +0530

    sc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import SideNav from './components/SideNav/SideNav';
+import Welcome from './components/Welcome/Welcome';
+import Routes from './routes';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      appName: 'Mine App'
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Header name={this.state.appName} />
+        <div>
+          <Welcome />
+        </div>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
